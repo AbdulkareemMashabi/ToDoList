9b712f3053af62d7cec6982fd581388c0fc25115
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-07 21:40:16 +0300

    fix: handle lottie page

---
+++ Screens/Lottie/Lottie.js
@@ -0,0 +1,22 @@
+import {View, StyleSheet} from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export const Lottie = ({navigation}) => {
+  return (
+    <View style={styles.takingAllPage}>
+      <LottieView
+        source={require('../../assets/lottie.json')}
+        autoPlay
+        loop={false}
+        onAnimationFinish={() => navigation.replace('Dashboard')}
+        style={styles.takingAllPage}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  takingAllPage: {flex: 1},
+});
+
+export default Lottie;
