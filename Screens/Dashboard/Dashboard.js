b2915d2c88ff33fc013816bf9a783ae396481300
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-07 21:40:39 +0300

    feat: handle dashboard page

---
+++ Screens/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import {StyleSheet, View} from 'react-native';
+
+export const Dashboard = () => {
+  return <View style={styles.takingAllPage}></View>;
+};
+
+const styles = StyleSheet.create({
+  takingAllPage: {flex: 1},
+});
+
+export default Dashboard;
