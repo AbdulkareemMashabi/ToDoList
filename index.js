d5c2d4fe02d4de463dea9d04e646c8fac9fc6605
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-13 21:45:05 +0300

    feat: config translation

--- index.js
+++ index.js
@@ -8,5 +8,18 @@ if (__DEV__) {
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {NativeModules} from 'react-native';
+import Locale from './helpers/localization';
+import en from './Language/en.json';
+import ar from './Language/ar.json';
+
+const deviceLanguage =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+    : NativeModules.I18nManager.localeIdentifier;
+const lang = deviceLanguage.slice(0, 2);
+Locale.config({en: en, ar: ar});
+Locale.setLanguage(lang);
 
 AppRegistry.registerComponent(appName, () => App);
