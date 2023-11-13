7ee6aac22e6d8769d695dd1f1e17319181ee0d3a
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-13 21:46:32 +0300

    feat: adding new pages

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Reactotron from 'reactotron-react-native';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import Lottie from './Screens/Lottie/Lottie';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, NativeModules} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Dashboard from './Screens/Dashboard/Dashboard';
+import Locale from './helpers/localization';
+import Login from './Screens/Login/Login';
+import {Icons} from './assets/Icons';
 
 const Stack = createStackNavigator();
 
@@ -21,13 +24,29 @@ export const App = () => {
             screenOptions={{
               headerStyle: styles.headerPageTitle,
               headerTitleAlign: 'center',
+              cardStyle: styles.pageStyle,
             }}>
             <Stack.Screen
               name="Lottie"
               component={Lottie}
-              options={{headerShown: false}}
+              options={{
+                headerShown: false,
+              }}
             />
-            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen
+              name="Dashboard"
+              component={Dashboard}
+              options={{
+                headerTitle: Locale.t('myWishesPage.myWishes'),
+              }}
+            />
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{
+                headerTitle: Locale.t('backupPage.backupTitle'),
+              }}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </GestureHandlerRootView>
@@ -42,9 +61,11 @@ const styles = StyleSheet.create({
     elevation: 0,
     shadowOpacity: 0,
   },
+  headerTitle: {fontSize: 17, fontWeight: 600},
   gestureStyle: {
     flex: 1,
   },
+  pageStyle: {paddingHorizontal: 16, paddingBottom: 16},
 });
 
 export default App;
