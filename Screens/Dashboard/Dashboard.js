f3354cf9363a84145f9b063ce18568cdc81b8528
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-13 21:47:06 +0300

    feat: dashboard page

--- Screens/Dashboard/Dashboard.js
+++ Screens/Dashboard/Dashboard.js
@@ -1,11 +1,46 @@
-import {StyleSheet, View} from 'react-native';
+import {useEffect} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import Button from '../../Components/Button';
+import {Icons} from '../../assets/Icons';
+import EmptyList from '../../Components/EmptyList';
+import {Images} from '../../assets/Images';
 
-export const Dashboard = () => {
-  return <View style={styles.takingAllPage}></View>;
+export const Dashboard = ({navigation}) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          source={Icons.cloud}
+          onPress={() => {
+            navigation.push('Login');
+          }}
+        />
+      ),
+    });
+  }, [navigation]);
+  return (
+    <View style={styles.takingAllPage}>
+      {/* <FlatList /> */}
+      <EmptyList
+        image={Images.emptyListPic}
+        title={'myWishesPage.emptyFormTitle'}
+        description={'myWishesPage.emptyFormTDescription'}
+      />
+      <Button containerStyle={styles.button} source={Icons.plus} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   takingAllPage: {flex: 1},
+  button: {
+    width: 64,
+    height: 64,
+    alignSelf: 'flex-end',
+    backgroundColor: '#32ADE6',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
 });
 
 export default Dashboard;
