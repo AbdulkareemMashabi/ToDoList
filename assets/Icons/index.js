1d4b101bb7a2a0b835c19e9311af894f9b284a41
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-18 21:02:18 +0300

    fix: adding images and icons

--- assets/Icons/index.js
+++ assets/Icons/index.js
@@ -2,6 +2,7 @@ const Icons = {
   cloud: require('./cloud.png'),
   plus: require('./plus.png'),
   backButton: require('./backButton.png'),
+  logOut: require('./logOut.png'),
 };
 
 export {Icons};
--- assets/Images/index.js
+++ assets/Images/index.js
@@ -1,5 +1,6 @@
 const Images = {
   emptyListPic: require('./emptyListPic.png'),
+  waves: require('./waves.png'),
 };
 
 export {Images};
