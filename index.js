8ee94bab222363b4c6a3628ba31fc7ec75c93949
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-22 21:04:14 +0300

    fix: enhance the index file

--- index.js
+++ index.js
@@ -6,26 +6,15 @@ if (__DEV__) {
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
-import {NativeModules} from 'react-native';
-import Locale from './helpers/localization';
-import en from './Language/en.json';
-import ar from './Language/ar.json';
 import {Provider} from 'react-redux';
 import {store} from './helpers/Redux/store';
+import Root from './Root';
 
-const deviceLanguage =
-  Platform.OS === 'ios'
-    ? NativeModules.SettingsManager.settings.AppleLocale ||
-      NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
-    : NativeModules.I18nManager.localeIdentifier;
-const lang = deviceLanguage.slice(0, 2);
-Locale.config({en: en, ar: ar});
-Locale.setLanguage(lang);
-
-AppRegistry.registerComponent(appName, () => () => (
+const rootComponent = () => (
   <Provider store={store}>
-    <App />
+    <Root />
   </Provider>
-));
+);
+
+AppRegistry.registerComponent(appName, () => rootComponent);
