fc268e66b61cce4919848d8531583b8f06fe068e
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-22 21:07:08 +0300

    fix: adding language button

--- Screens/Dashboard/Dashboard.js
+++ Screens/Dashboard/Dashboard.js
@@ -1,23 +1,45 @@
 import {useEffect} from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {StyleSheet, Text, View, FlatList, Alert} from 'react-native';
 import Button from '../../Components/Button';
 import {Icons} from '../../assets/Icons';
 import EmptyList from '../../Components/EmptyList';
 import {Images} from '../../assets/Images';
 import {useDispatch} from 'react-redux';
 import {setPage} from '../../helpers/Redux/currentPageReducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Locale from '../../helpers/localization';
+import RNRestart from 'react-native-restart';
 
 export const Dashboard = ({navigation}) => {
   const dispatch = useDispatch();
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Button
-          source={Icons.cloud}
-          onPress={() => {
-            navigation.push('Login');
-          }}
-        />
+        <View style={styles.buttonsContainer}>
+          <Button
+            source={Icons.language}
+            onPress={async () => {
+              try {
+                await AsyncStorage.setItem(
+                  'language',
+                  Locale.language === 'ar' ? 'en' : 'ar',
+                );
+                RNRestart.restart();
+              } catch (e) {
+                Alert.alert(
+                  Locale.t('common.errorOccurred'),
+                  Locale.t('myWishesPage.languageErrorChange'),
+                );
+              }
+            }}
+          />
+          <Button
+            source={Icons.cloud}
+            onPress={() => {
+              navigation.push('Login');
+            }}
+          />
+        </View>
       ),
     });
     dispatch(setPage('Dashboard'));
@@ -44,6 +66,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#32ADE6',
     borderRadius: 16,
     justifyContent: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '70%',
   },
 });
 
