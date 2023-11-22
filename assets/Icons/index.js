1490a06a63e1211f8ecf861a6cd84b2668bbfdeb
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-22 21:03:16 +0300

    feat: adding new icons

--- assets/Icons/index.js
+++ assets/Icons/index.js
@@ -3,6 +3,9 @@ const Icons = {
   plus: require('./plus.png'),
   backButton: require('./backButton.png'),
   logOut: require('./logOut.png'),
+  eye: require('./eye.png'),
+  language: require('./language.png'),
+  eyeOff: require('./eyeOff.png'),
 };
 
 export {Icons};
