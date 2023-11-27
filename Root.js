40a575e1ae5e5280aaa03fe88c43edde4fc976c0
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-11-27 22:05:07 +0300

    fix: handeling language in root

--- Root.js
+++ Root.js
@@ -1,22 +1,35 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import Locale from './helpers/localization';
 import en from './Language/en.json';
 import ar from './Language/ar.json';
 import App from './App';
-import {Alert} from 'react-native';
+import {Alert, I18nManager} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import RNRestart from 'react-native-restart';
 
 export const Root = () => {
+  const [startApp, setStartApp] = useState(false);
   useEffect(() => {
     getUserLanguage();
   }, []);
+
+  const supportRTL = () => {
+    I18nManager.forceRTL(true);
+    RNRestart.restart();
+  };
 
   const getUserLanguage = async () => {
     try {
       Locale.config({en: en, ar: ar});
       const value = await AsyncStorage.getItem('language');
-      if (value !== null) Locale.setLanguage(value);
-      else Locale.setLanguage('ar');
+      if (value !== null) {
+        Locale.setLanguage(value);
+      } else {
+        Locale.setLanguage('ar');
+        await AsyncStorage.setItem('language', 'ar');
+        supportRTL();
+      }
+      setStartApp(true);
     } catch (e) {
       Alert.alert(
         Locale.t('common.errorOccurred'),
@@ -25,7 +38,7 @@ export const Root = () => {
     }
   };
 
-  return <App />;
+  return startApp ? <App /> : <></>;
 };
 
 export default Root;
