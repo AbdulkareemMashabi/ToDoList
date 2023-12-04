2c54fb012231487dd8355caf83b755795870999e
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-12-04 20:15:00 +0300

    feat: create utils files

---
+++ helpers/utils.js
@@ -0,0 +1,9 @@
+export const pagesNames = {
+  lottie: 'Lottie',
+  dashboard: 'Dashboard',
+  login: 'Login',
+  register: 'Register',
+  forgetPassword: 'ForgetPassword',
+};
+
+export const pagesUseWaveImage = ['Login', 'Register', 'ForgetPassword'];
