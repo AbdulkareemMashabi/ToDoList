62be09a524244ebae52c57762568c338e84631a9
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-12-04 20:17:21 +0300

    fix: using image background

--- App.js
+++ App.js
@@ -1,54 +1,105 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import Reactotron from 'reactotron-react-native';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import Lottie from './Screens/Lottie/Lottie';
-import {SafeAreaView, StyleSheet, Keyboard} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Keyboard,
+  ImageBackground,
+} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Dashboard from './Screens/Dashboard/Dashboard';
 import Login from './Screens/Login/Login';
-import {useSelector} from 'react-redux';
+import ForgetPassword from './Screens/ForgetPassword/ForgetPassword';
+import Register from './Screens/Register/Register';
+import {pagesNames, pagesUseWaveImage} from './helpers/utils';
+import {Images} from './assets/Images';
 
 const Stack = createStackNavigator();
 
 export const App = () => {
-  const {page} = useSelector(state => state.paging) || {};
+  const [currentPage, setCurrentPage] = useState(pagesNames.lottie);
 
   return (
     <SafeAreaView
-      style={[styles.safeAreaView, page === 'Lottie' ? styles.greyColor : null]}
+      style={[
+        styles.safeAreaView,
+        currentPage === pagesNames.lottie ? styles.greyColor : null,
+      ]}
       onStartShouldSetResponder={() => {
         Keyboard.dismiss();
       }}>
       <GestureHandlerRootView style={styles.gestureStyle}>
-        <NavigationContainer
-          theme={{
-            ...DefaultTheme,
-            colors: {
-              ...DefaultTheme.colors,
-              background: 'transparent',
-            },
-          }}>
-          <Stack.Navigator
-            initialRouteName="Lottie"
-            screenOptions={{
-              headerStyle: styles.headerPageTitle,
-              headerTitleAlign: 'center',
-              cardStyle: styles.pageStyle,
+        <ImageBackground
+          style={styles.image}
+          source={
+            pagesUseWaveImage.includes(currentPage) ? Images.waves : null
+          }>
+          <NavigationContainer
+            theme={{
+              ...DefaultTheme,
+              colors: {
+                ...DefaultTheme.colors,
+                background: 'transparent',
+              },
             }}>
-            <Stack.Screen
-              name="Lottie"
-              component={Lottie}
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen name="Dashboard" component={Dashboard} />
-            <Stack.Screen name="Login" component={Login} />
-          </Stack.Navigator>
-        </NavigationContainer>
+            <Stack.Navigator
+              initialRouteName={pagesNames.lottie}
+              screenOptions={{
+                headerStyle: styles.headerPageTitle,
+                headerTitleAlign: 'center',
+                cardStyle: styles.pageStyle,
+              }}>
+              <Stack.Screen
+                name={pagesNames.lottie}
+                component={Lottie}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name={pagesNames.dashboard}
+                component={Dashboard}
+                listeners={{
+                  focus: () => {
+                    setCurrentPage(pagesNames.dashboard);
+                  },
+                }}
+              />
+              <Stack.Screen
+                name="Login"
+                component={Login}
+                listeners={{
+                  focus: () => {
+                    setCurrentPage(pagesNames.login);
+                  },
+                }}
+              />
+              <Stack.Screen
+                name="ForgetPassword"
+                component={ForgetPassword}
+                listeners={{
+                  focus: () => {
+                    setCurrentPage(pagesNames.forgetPassword);
+                  },
+                }}
+              />
+              <Stack.Screen
+                name="Register"
+                component={Register}
+                listeners={{
+                  focus: () => {
+                    setCurrentPage(pagesNames.register);
+                  },
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </ImageBackground>
       </GestureHandlerRootView>
     </SafeAreaView>
   );
@@ -71,6 +122,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
   greyColor: {backgroundColor: '#e5e5e5'},
+  image: {flex: 1},
 });
 
 export default App;
