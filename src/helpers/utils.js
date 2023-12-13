9c00ed32b90ff358dc2afdbdb2fbcd15a5fd57a3
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-12-13 14:56:15 +0300

    fix: change file location

---
+++ src/helpers/utils.js
@@ -0,0 +1,55 @@
+import {Alert, Image, View} from 'react-native';
+import Locale from './localization';
+import {Icons} from '../assets/Icons';
+import Text from '../Components/Text';
+import Toast from 'react-native-toast-message';
+
+export const pagesNames = {
+  lottie: 'Lottie',
+  dashboard: 'Dashboard',
+  login: 'Login',
+  register: 'Register',
+  forgetPassword: 'ForgetPassword',
+};
+
+export const pagesUseWaveImage = ['Login', 'Register', 'ForgetPassword'];
+
+const errorMessages = {
+  'email-already-in-use': 'APIErrorMessages.emailUsed',
+  'weak-password': 'APIErrorMessages.weakPassword',
+};
+
+export const handleAPIErrors = error => {
+  const message = errorMessages[error.code.split('/')[1]];
+  Alert.alert(Locale.t('common.errorOccurred'), Locale.t(message));
+};
+
+export const toastConfig = {
+  success: props => (
+    <View
+      style={{
+        flexDirection: 'row',
+        backgroundColor: '#30D158',
+        alignItems: 'center',
+        borderRadius: 16,
+        paddingVertical: 6,
+        paddingHorizontal: 10,
+        marginHorizontal: 20,
+      }}>
+      <Image source={Icons.checkMark} style={{marginHorizontal: 10}} />
+      <Text
+        style={{color: 'white'}}
+        localeKey={props.text1}
+        variant="bodySemibold"
+      />
+    </View>
+  ),
+};
+
+export const showToast = text => {
+  Toast.show({
+    type: 'success',
+    position: 'top',
+    text1: text,
+  });
+};
