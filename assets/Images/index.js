4be115500112c59243ab6f3a4faa6277702a9bb0
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-12-13 14:09:11 +0300

    fix: code enhancements

--- assets/Images/index.js
+++
@@ -1,6 +0,0 @@
-const Images = {
-  emptyListPic: require('./emptyListPic.png'),
-  waves: require('./waves.png'),
-};
-
-export {Images};
--- helpers/utils.js
+++
@@ -1,9 +0,0 @@
-export const pagesNames = {
-  lottie: 'Lottie',
-  dashboard: 'Dashboard',
-  login: 'Login',
-  register: 'Register',
-  forgetPassword: 'ForgetPassword',
-};
-
-export const pagesUseWaveImage = ['Login', 'Register', 'ForgetPassword'];
