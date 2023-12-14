b07ae7ebdb0556fae28cc476e93a3774e24dd4e7
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-12-14 16:46:36 +0300

    fix: enhance reading firestore object from env file

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBT3xgCdgYoUYWHnVj1AnbWy3_SOZiUvys',
-  authDomain: 'to-do-list-e2a37.firebaseapp.com',
-  projectId: 'to-do-list-e2a37',
-  storageBucket: 'to-do-list-e2a37.appspot.com',
-  messagingSenderId: '206358385320',
-  appId: '1:206358385320:web:c6e27861430f136779e89d',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
