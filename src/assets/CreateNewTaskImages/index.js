1003d8d850f9330ceed2bab03711a1f9c65461b8
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-12-20 16:20:59 +0300

    feat: add new images

---
+++ src/assets/CreateNewTaskImages/index.js
@@ -0,0 +1,8 @@
+const CreateNewTaskImages = {
+  blue: require('./blue.png'),
+  red: require('./red.png'),
+  green: require('./green.png'),
+  yellow: require('./yellow.png'),
+};
+
+export {CreateNewTaskImages};
