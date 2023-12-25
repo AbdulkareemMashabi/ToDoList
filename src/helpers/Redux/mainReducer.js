94ea46b1f1b76cf9642173f8e59a996494ace254
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2023-12-25 17:02:24 +0300

    feat: addingbackgroundColor

--- src/helpers/Redux/mainReducer.js
+++ src/helpers/Redux/mainReducer.js
@@ -3,6 +3,7 @@ import {createSlice} from '@reduxjs/toolkit';
 const initialState = {
   isLoading: false,
   userId: null,
+  backgroundColor: null,
 };
 
 export const LoadingSlice = createSlice({
@@ -15,9 +16,13 @@ export const LoadingSlice = createSlice({
     setUserId: (state, action) => {
       state.userId = action.payload;
     },
+    setBackgroundColor: (state, action) => {
+      state.backgroundColor = action.payload;
+    },
   },
 });
 
-export const {setIsLoading, setUserId} = LoadingSlice.actions;
+export const {setIsLoading, setUserId, setBackgroundColor} =
+  LoadingSlice.actions;
 
 export default LoadingSlice.reducer;
