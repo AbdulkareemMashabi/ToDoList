0b13c55e3c826f21a4cd01ab176387b690e53b14
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2024-01-08 20:47:21 +0300

    feat: adding new component and its style

---
+++ src/Components/DoubleText/DoubleText.js
@@ -0,0 +1,43 @@
+import {View} from 'react-native';
+import Text from '../Text/Text';
+import styles from './DoubleText.style';
+import Button from '../Button/Button';
+import {Icons} from '../../assets/Icons';
+
+export const DoubleText = ({
+  title,
+  description,
+  date,
+  editButtonPress,
+  deleteButtonPress,
+}) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text value={title} />
+        <Text
+          value={description}
+          variant="bodyRegular"
+          style={styles.description}
+        />
+      </View>
+      <View style={styles.container}>
+        <Text value={date} variant="bodyRegular" isGrey />
+        <View style={styles.buttonContainer}>
+          {editButtonPress ? (
+            <Button source={Icons.edit} onPress={editButtonPress} />
+          ) : null}
+          {deleteButtonPress ? (
+            <Button
+              source={Icons.delete}
+              containerStyle={styles.button}
+              onPress={deleteButtonPress}
+            />
+          ) : null}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default DoubleText;
---
+++ src/Components/DoubleText/DoubleText.style.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  description: {
+    marginTop: 16,
+  },
+  button: {
+    marginTop: 16,
+  },
+  buttonContainer: {
+    marginLeft: 16,
+  },
+});
