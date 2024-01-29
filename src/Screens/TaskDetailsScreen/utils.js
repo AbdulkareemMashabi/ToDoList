fcab66ad4c16d900a790ea5ed4ce8a5fca9bf1c1
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2024-01-29 21:03:40 +0300

    feat: adding new function to handle fields

--- src/Screens/TaskDetailsScreen/utils.js
+++ src/Screens/TaskDetailsScreen/utils.js
@@ -17,3 +17,30 @@ export const getInitialValues = (formData, selectedIndex) => {
       : formData?.mainTask?.date,
   };
 };
+
+export const getFormFields = (formData, selectedIndex) => {
+  const formFields = [
+    {type: 'TextField', name: 'title', label: 'newTask.title'},
+  ];
+  if (formData?.subTasks?.length === selectedIndex) {
+    const dataObject = formData?.mainTask?.date?.split('/');
+    const finalDataObject = new Date(
+      moment(`${dataObject[2]}-${dataObject[1]}-${dataObject[0]}`).format(
+        'YYYY-MM-DD',
+      ),
+    );
+    formFields.push({
+      type: 'DatePicker',
+      name: 'date',
+      label: 'newTask.date',
+      maximumDate: finalDataObject,
+    });
+  }
+  formFields.push({
+    type: 'TextArea',
+    name: 'description',
+    label: 'newTask.description',
+  });
+
+  return formFields;
+};
