1cbe26ddf671ccb145cdef0fa49db61be62cf113
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2024-02-17 16:28:24 +0300

    feat: adding new variant

--- src/Components/Text/Text.style.js
+++ src/Components/Text/Text.style.js
@@ -5,6 +5,7 @@ export default StyleSheet.create({
   bodySemibold: {fontSize: 17, fontWeight: '600'},
   bodyRegular: {fontSize: 17, fontWeight: '400'},
   subHead: {fontSize: 15, fontWeight: '400'},
+  captionSemibold: {fontSize: 13, fontWeight: '600'},
   captionRegular: {color: '#FC5555', fontSize: 13, fontWeight: '400'},
   greyColor: {color: '#72788E'},
   blueColor: {color: '#32ADE6'},
