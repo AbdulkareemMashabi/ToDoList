028887baa13853255c2982d79bbef4dd5c4afc29
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2024-02-17 16:22:56 +0300

    feat: adding scrollable and loading

--- src/Components/Contianer/Container.js
+++ src/Components/Contianer/Container.js
@@ -1,8 +1,33 @@
-import {View} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import styles from './Container.style';
+import Skeleton from '../Skeleton/Skeleton';
 
-export const Container = ({children, noPadding, style}) => {
-  return (
+export const Container = ({
+  children,
+  noPadding,
+  style,
+  scrollable,
+  isLoading,
+  SkeletonPadding,
+}) => {
+  if (isLoading)
+    return (
+      <View
+        style={[
+          styles.container,
+          noPadding && !SkeletonPadding ? styles.noPadding : null,
+          style,
+        ]}>
+        <Skeleton />
+      </View>
+    );
+
+  return scrollable ? (
+    <ScrollView
+      style={[styles.container, noPadding ? styles.noPadding : null, style]}>
+      {children}
+    </ScrollView>
+  ) : (
     <View
       style={[styles.container, noPadding ? styles.noPadding : null, style]}>
       {children}
