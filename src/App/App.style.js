16432e1b1786819e7b6be0d34725c54faf260c0e
a.mashabi <abdulkareemmashabi@Abdulkareems-MacBook-Pro.local>
2024-05-05 23:26:03 +0300

    feat: remove uneeded code

--- src/App/App.style.js
+++ src/App/App.style.js
@@ -1,22 +1,9 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-  safeAreaView: {flex: 1},
   headerTitle: {fontSize: 17, fontWeight: 600},
-  gestureStyle: {
-    flex: 1,
-  },
   pageStyle: {
     backgroundColor: 'transparent',
-  },
-  greyColor: {backgroundColor: '#e5e5e5'},
-  image: {flex: 1, backgroundColor: 'white'},
-  takingAllPage: {width: '100%', flex: 1},
-  lottieView: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   title: {
     width: '50%',
--- src/App/utils.js
+++ src/App/utils.js
@@ -18,16 +18,3 @@ export const getUserData = async (userId, dispatch) => {
       dispatch(setIsLoading(false));
     }
 };
-
-export const getRandomNumber = usedNumbers => {
-  let randomNumber = Math.floor(Math.random() * 4);
-  if (usedNumbers.current.length === 4) {
-    usedNumbers.current = [];
-  }
-  while (usedNumbers.current.includes(randomNumber)) {
-    randomNumber = Math.floor(Math.random() * 4);
-  }
-  usedNumbers.current.push(randomNumber);
-
-  return randomNumber;
-};
