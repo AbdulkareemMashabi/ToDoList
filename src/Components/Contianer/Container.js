40c1cc47d2df077e0c8662e3b35c2e1da3285b7f
a.mashabi <abdulkareemmashabi@Abdulkareems-MacBook-Pro.local>
2024-05-05 23:40:43 +0300

    feat: enhance container with its style

--- src/Components/Contianer/Container.js
+++ src/Components/Contianer/Container.js
@@ -1,42 +1,88 @@
-import {KeyboardAvoidingView, Platform, ScrollView, View} from 'react-native';
+import {
+  ImageBackground,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import styles from './Container.style';
 import Skeleton from '../Skeleton/Skeleton';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Toast from 'react-native-toast-message';
+import LottieView from 'lottie-react-native';
+import {toastConfig} from '../../helpers/utils';
+import {useSelector} from 'react-redux';
+import {Images} from '../../assets/Images';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Button from '../Button/Button';
 
 export const Container = ({
   children,
   noPadding,
   style,
   scrollable,
-  isLoading,
-  SkeletonPadding,
+  isLoading: _isLoading,
+  backgroundImage,
+  renderFooter,
+  backgroundColor,
 }) => {
+  const {isLoading, isLoadingOverLay} = useSelector(state => state.main);
   const behavior = Platform.OS === 'ios' ? 'padding' : 'height';
-
-  if (isLoading)
-    return (
-      <View
-        style={[
-          styles.container,
-          noPadding && !SkeletonPadding ? styles.noPadding : null,
-          style,
-        ]}>
-        <Skeleton />
-      </View>
-    );
+  const insets = useSafeAreaInsets();
 
   const renderContent = () => {
-    if (scrollable)
-      return <ScrollView style={styles.flex_1}>{children}</ScrollView>;
-    else return <View style={styles.flex_1}>{children}</View>;
+    if (_isLoading) return <Skeleton padding={noPadding} />;
+    else if (scrollable)
+      return <ScrollView style={styles.renderContent}>{children}</ScrollView>;
+    else return <View style={styles.renderContent}>{children}</View>;
+  };
+
+  const renderFooterContent = () => {
+    return (
+      <View style={styles.renderFooter}>
+        <Button {...renderFooter} />
+      </View>
+    );
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={behavior}
-      keyboardVerticalOffset={50}
-      style={[styles.container, noPadding ? styles.noPadding : null, style]}>
-      {renderContent()}
-    </KeyboardAvoidingView>
+    <ImageBackground
+      style={[styles.flex_1, styles.image]}
+      source={backgroundImage || Images.light}>
+      <SafeAreaView
+        edges={['top', 'left', 'right']}
+        pointerEvents={isLoading || isLoadingOverLay ? 'none' : 'auto'}
+        style={[
+          styles.flex_1,
+          {paddingTop: insets.top, backgroundColor: backgroundColor},
+        ]}
+        onStartShouldSetResponder={() => {
+          Keyboard.dismiss();
+        }}>
+        <GestureHandlerRootView style={styles.flex_1}>
+          <KeyboardAvoidingView
+            behavior={behavior}
+            keyboardVerticalOffset={50}
+            style={[styles.container, style]}>
+            {renderContent()}
+            {renderFooter && renderFooterContent()}
+          </KeyboardAvoidingView>
+        </GestureHandlerRootView>
+        <Toast config={toastConfig} />
+        {isLoadingOverLay ? (
+          <View style={styles.lottieView}>
+            <LottieView
+              source={require('../../assets/Lottie/loadingOverLay.json')}
+              autoPlay
+              loop
+              style={styles.takingAllPage}
+            />
+          </View>
+        ) : null}
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
--- src/Components/Contianer/Container.style.js
+++ src/Components/Contianer/Container.style.js
@@ -1,15 +1,35 @@
 import {StyleSheet} from 'react-native';
+import {cardShadow} from '../../helpers/shadow';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    marginTop: 48,
   },
   noPadding: {
     padding: 0,
   },
   flex_1: {
+    flex: 1,
+  },
+  greyColor: {backgroundColor: '#e5e5e5'},
+  image: {backgroundColor: 'white'},
+  takingAllPage: {width: '100%', flex: 1},
+  lottieView: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  renderFooter: {
+    ...cardShadow,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    padding: 16,
+    paddingVertical: 32,
+  },
+  renderContent: {
+    paddingHorizontal: 16,
     flex: 1,
   },
 });
