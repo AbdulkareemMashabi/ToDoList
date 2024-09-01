bece7a7b65eaf812c97c8c3ae0610cdf70dd6715
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2024-09-01 21:10:07 +0300

    feat: for clicking

--- android/app/src/main/java/com/todolist/ToDoListWidget.java
+++ android/app/src/main/java/com/todolist/ToDoListWidget.java
@@ -2,9 +2,11 @@ package com.todolist;
 
 
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
@@ -56,6 +58,12 @@ public class ToDoListWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Get the layout for the widget and attach the click listener
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.to_do_list_widget);
+        views.setOnClickPendingIntent(R.id.root, pendingIntent);
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             try {
