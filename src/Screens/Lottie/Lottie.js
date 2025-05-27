b69db059490732ad21beabf0b9c37dd4d3e5c84e
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2025-05-27 18:05:12 +0300

    feat: remove unused utils

--- src/Screens/Lottie/Lottie.js
+++ src/Screens/Lottie/Lottie.js
@@ -9,12 +9,7 @@ import {Alert, I18nManager} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RNRestart from 'react-native-restart';
 import {useDispatch} from 'react-redux';
-import {
-  setIsDeviceId,
-  setIsLoading,
-  setToken,
-  setUserId,
-} from '../../helpers/Redux/mainReducer';
+import {setIsLoading, setToken} from '../../helpers/Redux/mainReducer';
 import BootSplash from 'react-native-bootsplash';
 
 import styles from './Lottie.style';
